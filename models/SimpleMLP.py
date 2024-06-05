b72d665165cd5206d1831e2b87d9a667df67c5db
j-milkovits <j.milkovits.t@posteo.net>
2024-06-05 16:52:24 +0200

    add simple mlp

---
+++ models/SimpleMLP.py
@@ -0,0 +1,20 @@
+from torch.nn import Linear, Module, ReLU, Sequential, Sigmoid
+
+
+class SimpleMLP(Module):
+    def __init__(self):
+        super(SimpleMLP, self).__init__()
+        self.mlp_stack = Sequential(
+            Linear(300, 600),
+            ReLU(),
+            Linear(600, 300),
+            ReLU(),
+            Linear(300, 300),
+            ReLU(),
+            Linear(300, 1),
+            Sigmoid(),  # binary classification
+        )
+
+    def forward(self, x):
+        x = self.mlp_stack(x)
+        return x
