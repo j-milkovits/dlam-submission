27224227afa6a1a2047b6bb75ab8b2324cd7e02b
j-milkovits <j.milkovits.t@posteo.net>
2024-06-05 16:33:00 +0200

    add functionality to load disaster dataset

---
+++ data/DisasterDataset.py
@@ -0,0 +1,54 @@
+from typing import Callable, Tuple, TypedDict
+
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+class Sample(TypedDict):
+    tweet: list[float]
+    target: int
+
+
+class DisasterDataset(Dataset):
+    def __init__(
+        self,
+        tweets: list[str],
+        targets: list[int],
+        emb_fn: Callable[[list[str]], list[float]],
+        tokenizer: Callable[[str], list[str]],
+    ) -> None:
+        self.data: list[Sample] = []
+        assert len(tweets) == len(
+            targets
+        ), "The arrays tweets and targets should have the same length."
+        zipped_items = zip(tweets, targets)
+        for tweet, target in zipped_items:
+            self.data.append(
+                {
+                    # tokenize and embed sentence
+                    "tweet": emb_fn(tokenizer(tweet)),
+                    "target": target,
+                }
+            )
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+def load_disaster_train_dataset(path: str) -> Tuple[
+    list[str],
+    list[int],
+]:
+    df = pd.read_csv(path, sep=",")
+    return (
+        list(df["text"].values),
+        list(df["target"].values),
+    )
+
+
+def load_disaster_test_dataset(path: str) -> list[str]:
+    df = pd.read_csv(path, sep=",")
+    return list(df["text"].values)
